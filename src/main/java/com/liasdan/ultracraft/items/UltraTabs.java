d786a8e1fea251eeb6fc8f86bb8eab09ae8a31cc
LiasDan <liasdan22@gmail.com>
2025-06-27 13:39:07 +0800

    another test

--- src/main/java/com/liasdan/ultracraft/items/UltraTabs.java
+++ src/main/java/com/liasdan/ultracraft/items/UltraTabs.java
@@ -60,48 +60,12 @@ public class UltraTabs {
 		}
 		else if(event.getTab() == UltraTabs.MiscTab.get()) {
 
-			event.accept(MobsCore.ZOLDERS_SPAWN_EGG);
-
-			event.accept(MobsCore.CRIMERS_SPAWN_EGG);
-
-			event.accept(MobsCore.CUTMEN_SPAWN_EGG);
-
-			event.accept(MobsCore.MACHINEMEN_SPAWN_EGG);
-
-			event.accept(MobsCore.UNGLERS_SPAWN_EGG);
-
-			event.accept(MobsCore.WUMPERS_SPAWN_EGG);
-			event.accept(MobsCore.SIGNALMAN_EVIL_SPAWN_EGG);
-
-			event.accept(MobsCore.YARTOTS_SPAWN_EGG);
-			event.accept(MobsCore.BLACK_KNIGHT_SPAWN_EGG);
-
-			event.accept(MobsCore.ORGETTES_SPAWN_EGG);
-			event.accept(MobsCore.DUKE_ORG_ROUKI_SPAWN_EGG);
-
-			event.accept(MobsCore.NANASHIS_SPAWN_EGG);
-			event.accept(MobsCore.GEDOU_SHINKEN_RED_SPAWN_EGG);
-
-			event.accept(MobsCore.PORDERMEN_SPAWN_EGG);
-
-			event.accept(MobsCore.DRUNNS_SPAWN_EGG);
-			event.accept(MobsCore.GAISOULG_SPAWN_EGG);
-			event.accept(MobsCore.RYUSOUL_MORIA_SPAWN_EGG);
-
-			event.accept(MobsCore.NEJIRETTAS_SPAWN_EGG);
+			//event.accept(MobsCore.ZOLDERS_SPAWN_EGG);
 			
 			for (int i = 0; i < UltraTabs.MISC.size(); i++)
 			{
 				event.accept( UltraTabs.MISC.get(i));
 			}
-
-			event.accept(OtherItems.SUSUME_GORANGER_MUSIC_DISC);
-
-			event.accept(OtherItems.TAIYOU_SENTAI_SUN_VULCAN_MUSIC_DISC);
-
-			event.accept(OtherItems.GAORANGER_HOERO_MUSIC_DISC);
-
-			event.accept(OtherItems.SAMURAI_SENTAI_SHINKENGER_MUSIC_DISC);
 		}
 		else if(event.getTab() == UltraTabs.BlockTab.get()) {
 			for (int i = 0; i < UltraTabs.BLOCKS.size(); i++)
